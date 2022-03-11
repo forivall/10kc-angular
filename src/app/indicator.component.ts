ca0dac9106cfb22593d168147bc2b75004e7c6d7
forivall <forivall@gmail.com>
2022-03-11 14:14:00 -0800

    doc: clean up doc comments

--- src/app/indicator.component.ts
+++ src/app/indicator.component.ts
@@ -6,11 +6,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class IndicatorComponent {
   @Input() count: number | undefined;
-  /** To receive, this emits `true`, to stop, emits `false` */
+  /** To receive, this emits `true`. To stop, emits `false` */
   @Output() recieve = new EventEmitter<boolean>();
-  /** Reset button clicked */
   @Output() reset = new EventEmitter<void>();
-  
+
   isRecieving = false;
   constructor() {
     this.recieve.subscribe((value) => {
