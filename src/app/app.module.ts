e971b5f16fc0d8ac85d446690a35c8209288de01
Emily Marigold Klassen <forivall@gmail.com>
2022-03-10 17:45:07 -0800

    feat: use pink-bluegrey colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge'
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { IndicatorComponent } from './indicator.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, MatBadgeModule ],
+  declarations: [ AppComponent, IndicatorComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
